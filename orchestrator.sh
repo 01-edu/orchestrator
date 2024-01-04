1ade81d7d1f0576849823c8e1ca04108e4be2828
nprimo <primo.niccolo@gmail.com>
2024-01-04 09:35:17 +0000

    chore(orchestrator.sh): format and refactor multiline echo

--- orchestrator.sh
+++ orchestrator.sh
@@ -3,46 +3,48 @@
 KUBECONFIG="./k3s/k3s.yaml"
 
 function help() {
-    echo "orchestrator CLI v0.0.1"
-    echo
-    echo "Manage a Kubernetes cluster in a VM kluster running K3s"
-    echo
-    echo "USAGE"
-    echo "  $./orchestrator.sh COMMAND"
-    echo 
-    echo "Available commands:"
-    echo "  create  Create the VM cluster using the local Vagrantfile config"
-    echo "  start   Start the Kubernetes cluster on the VM cluster"
-    echo "  stop    Stop the Kubernetes cluster on the VM cluster"
-    echo
+	cat <<EOF
+orchestrator CLI v0.0.1
+
+Manage a Kubernetes cluster in a VM kluster running K3s
+
+USAGE"
+$./orchestrator.sh COMMAND
+
+Available commands:
+    create  Create the VM cluster using the local Vagrantfile config
+    start   Start the Kubernetes cluster on the VM cluster
+    stop    Stop the Kubernetes cluster on the VM cluster
+EOF
 }
 
 if [[ $# -ne 1 ]]; then
-    help
-    exit 1
+	help
+	exit 1
 fi
 
-case $1 in 
-    "create")
-        echo create resources
-        mkdir -p ./k3s
-        vagrant up
-        ;;
-    "start")
-        echo start cluster
-        KUBECONFIG=${KUBECONFIG} kubectl apply -f ./manifests/
-        ;;
-    "stop")
-        echo stop cluster
-        vagrant suspend
-        ;;
-    "clean")
-        echo remove cluster and resources
-        vagrant destroy -f 
-        ;;
-    *)
-        echo "${1} is an unknown command"
-        help
-        exit 1
-        ;;
+case $1 in
+"create")
+	echo create resources
+	mkdir -p ./k3s
+	vagrant up
+	;;
+"start")
+	echo start cluster
+	KUBECONFIG=${KUBECONFIG} kubectl apply -f ./manifests/
+	;;
+"stop")
+	echo stop cluster
+	vagrant suspend
+	;;
+"clean")
+	echo remove cluster and resources
+	vagrant destroy -f
+    rm -rf ./k3s
+	;;
+*)
+	echo "${1} is an unknown command"
+	help
+	exit 1
+	;;
 esac
